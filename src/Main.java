0e574101a9905529a1698b8b41344b6afdf6a320
Wpabloz <wesleypablo1311@gmail.com>
2025-05-22 21:55:46 -0300

    teste wesley

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Teste Branch");
+        System.out.println("Teste Branch 00");
         System.out.println("Teste Branch 2");
         System.out.println("Teste Branch 3");
         System.out.println("Teste Branch 4");
